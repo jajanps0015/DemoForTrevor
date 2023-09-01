09456eaa837aca1a5af62e5b37980b6c3aabaae1
jajanpreetsingh <jajanpreetsingh15@gmail.com>
2023-08-31 20:10:26 -0400

    update sdl libraries

---
+++ SDLProject/SDLProject/src/SDLProject.cpp
@@ -0,0 +1,12 @@
+// SDLProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <SDLMain.h>
+
+#define HUNDRED 100
+
+int main()
+{
+    std::cout << HUNDRED;
+}
