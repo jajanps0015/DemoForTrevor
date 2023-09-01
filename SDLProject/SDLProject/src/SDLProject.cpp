0c2e6391bcf65b545985586d7205a1bce54fb5da
jajanpreetsingh <jajanpreetsingh15@gmail.com>
2023-08-31 20:27:03 -0400

    update sl2 paths

--- SDLProject/SDLProject/src/SDLProject.cpp
+++ SDLProject/SDLProject/src/SDLProject.cpp
@@ -9,4 +9,15 @@
 int main()
 {
     std::cout << HUNDRED;
+
+	auto *window = SDL_CreateWindow(
+		"WINDOW_TITLE",				// window title
+		SDL_WINDOWPOS_UNDEFINED,	// window x pos
+		SDL_WINDOWPOS_UNDEFINED,	// window y pos
+		600,				// window width
+		800,				// window height
+		SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);			// window flags
+	if (window == nullptr) {
+		return false;
+	}
 }
